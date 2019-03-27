db4b8d104c2a5670c1081697d7959ce44ad50247
minhchinh97h <minhchinhduong97@gmail.com>
2019-03-27 23:11:46 +0200

    executed player and possibly related lover can be now eliminated from callingOrder

--- routes/InGame/actions/round-end/round-end.js
+++ routes/InGame/actions/round-end/round-end.js
@@ -117,18 +117,85 @@ router.post('/:roomid/request-hang-player', (req, res, next) => {
                         return true
                     })
 
-                    res.send("all players voted")
+                    
+                    //Update callingOrder into Rooms collection
+                    Room.updateOne({"roomid": req.params.roomid}, {$set: {"callingOrder": callingOrder}}, (err, result) => {
+                        if(err) return console.log(err)
+
+                        if(result !== null){
+                            res.send("all players voted")
+                        }
+                    })
                 }
 
-                else
-                    res.send("not all players voted")
+                else{
+                    //Update callingOrder into Rooms collection
+                    Room.updateOne({"roomid": req.params.roomid}, {$set: {"callingOrder": callingOrder}}, (err, result) => {
+                        if(err) return console.log(err)
 
-                //Update callingOrder into Rooms collection
+                        if(result !== null){
+                            res.send("not all players voted")
+                        }
+                    })
+                }
+            }
+        })
+    })
+})
+
+router.get('/:roomid/request-to-get-hang-player', (req, res, next) => {
+    mongoose.connect(mongoUrl, { useNewUrlParser: true })
+
+    var db = mongoose.connection
+
+    db.on('error', console.error.bind(console, 'connection error: '))
+
+    db.once('open', () => { 
+        Room.findOne({"roomid": req.params.roomid}, {"callingOrder": 1, "_id": 0}, (err, result) => {
+            if(err) return console.log(err)
+
+            if(result !== null){
+                let callingOrder = result.callingOrder,
+                    chosenTarget, //chosenTarget from round end target of callingOrder
+                    lovers = [],  //array holding players in case chosenTarget is in love with someone else
+                    isInLove = false //to determine whether the chosenTarget is in love
+
+                //Get chosenTarget
+                callingOrder.every((order, index) => {
+                    if(order.name === "round end target"){
+                        chosenTarget = order.chosenTarget
+                        return false
+                    }
+                    return true
+                })
+
+                //Check whether the hanged player is in love with anyone else
+                callingOrder.every((order, index) => {
+                    if(order.name === "The Lovers"){
+                        if(order.player instanceof Array && order.player.includes(chosenTarget)){
+                            lovers = order.player
+                            isInLove = true
+                        }
+                    }
+                })
+
+                if(isInLove){
+                    //Eliminate the hanged player and the related lover from callingOrder and update callingOrder
+                    lovers.forEach((player) => {
+                        EliminateTheHangedPlayerFromCallingInTurn(player)
+                    })
+                }
+
+                else{
+                    //Eliminate the hanged player from callingOrder and update callingOrder
+                    EliminateTheHangedPlayerFromCallingInTurn(chosenTarget)
+                }
+
                 Room.updateOne({"roomid": req.params.roomid}, {$set: {"callingOrder": callingOrder}}, (err, result) => {
                     if(err) return console.log(err)
 
                     if(result !== null){
-                        
+                        res.send(chosenTarget)
                     }
                 })
             }
@@ -136,13 +203,24 @@ router.post('/:roomid/request-hang-player', (req, res, next) => {
     })
 })
 
+function EliminateTheHangedPlayerFromCallingInTurn(chosenTarget){
+    callingOrder.forEach((order, index) => {
+        if(!order.special && order.player instanceof Array){
+            order.player.forEach((player, i, playerArr) => {
+                if(player === chosenTarget)
+                    playerArr.splice(i, 1)
+            })
+        }
+    })
+}
+
 module.exports = (io) => {
 
     let reIO = io.of('/round-end')
 
     reIO.setMaxListeners(Infinity)
 
-
+    //inside requestToHangPlayer, we calculate the chosenTarget which is the final hanged player and update to the database
     const requestToHangPlayer = (data) => {
         axios({
             method: 'post',
@@ -150,10 +228,14 @@ module.exports = (io) => {
             data: data
         })
         .then(res => {
-            //Make axios call to choose one from the executed players
+            //Make axios call to get the executed player and also make calculation to eliminate the player (and also related lover) from callingOrder
             if(res.data ===  "all players voted"){
-                return axios({
-                    method: ''
+                axios({
+                    method: 'get',
+                    url: 'http://localhost:3001/in-game/actions/' + data.roomid + '/request-to-get-hang-player',
+                })
+                .then(res => {
+                    
                 })
             }
         })
